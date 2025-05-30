70639021cc213babf2bc412b45c4e52142d7b8cd
Thetan1us <geeksa40@gmail.com>
2025-05-30 19:51:58 +0000

    Add player triangle rendering on a screen

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import pygame
 import constants
+from player import Player
 
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     dt = 0
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick() / 1000
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
